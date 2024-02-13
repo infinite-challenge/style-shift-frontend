4af3edfc68b4e65767f513cf472f6cba8a92c327
ykdy3951 <ykdy3951@naver.com>
2024-02-13 23:56:46 +0900

    Add header component

--- src/header.tsx
+++ src/header.tsx
@@ -1,20 +1,49 @@
 import React from 'react';
+import './header.css';
+
+import logo from './Assets/logo.svg';
+import setting from './Assets/setting.svg';
 
 export default function Header() {
     return (
-        <div className="header h-14 bg-gray-800">
-            <div className="header-container h-full px-7 flex items-center">
-                <div className="icon-logo-container font-bold text-white text-2xl">
-                    Style Shift
+        <div className="header header-container">
+            <div className="header-title-container">
+                <div className="header-title-icon">
+                    {/* logo image */}
+                    <img src={logo} className="header-title-icon-svg" alt="logo" />
                 </div>
-                {/* user-icon-container right */}
-                <div className="user-icon-container bg-black rounded-full h-8 w-8 ml-auto cursor-pointer">
-                    <img src="https://avatars.githubusercontent.com/u/1000000?s=460&u=4e4d9e8b2d0c5f7d9e8b2d0c5f7d4e8b2d0c5f7d&v=4" 
-                    alt="User Icon" 
-                    className="user-icon rounded-full duration-300 ease-in-out hover:opacity-50"
-                    />
+                <div className="header-title-text-container">
+                    <div className="header-title-text-box">
+                        <div className="header-title-text header-title-text-layout">
+                            AI Style Shift
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="header-function-container">
+                <div className="header-links-container">
+                    <div className="display-flex font-style item">
+                        Home
+                    </div>
+                </div>
+                {/* dark mode toggle */}
+                {/* <div className="header-dark-mode-toggle-container">
+                    <div className="header-dark-mode-toggle">
+                        <label className="switch">
+                            <input type="checkbox" />
+                            <span className="slider round"></span>
+                        </label>
+                    </div>
+                </div> */}
+                <div className="header-button-container">
+                <div className="header-button-icon-container">
+                    <div className="header-button-icon">
+                        <img src={setting} className="header-button-icon-svg" alt="setting" />
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
     );
 }
+
